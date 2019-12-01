951e151132bf7d2b46270ece6860da2a2c2a9115
HoangTran <40290572+hoangtran-97@users.noreply.github.com>
2019-12-01 10:59:17 +0200

    Testing animation

--- src/screens/Dashboard/components/channelSelector.js
+++ src/screens/Dashboard/components/channelSelector.js
@@ -1,25 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import {
-    Text, StyleSheet, Image, FlatList
+    Text, StyleSheet, Image, FlatList, View, TouchableOpacity
 } from "react-native";
+import { animated, useSpring } from "react-spring";
+import BezierEasing from "bezier-easing";
+import Icon from "react-native-vector-icons/FontAwesome5";
 
-const ChannelSelector = ({ data }) => (
-    <>
-        <FlatList
-            data={data}
-            renderItem={({ item }) => (
-                <>
-                    <Image
-                        style={{ width: 50, height: 50 }}
-                        source={{ uri: item.icon }}
-                    />
-                    <Text>{item.name}</Text>
-                </>
-            )}
-            keyExtractor={(item) => item.id}
-        />
-    </>
-);
+const AnimatedView = animated(View);
+
+const ChannelSelector = ({ data }) => {
+    const animation = useSpring({
+        backgroundColor: "green",
+        height: collapse ? 0 : 200,
+    });
+    const [collapse, setCollapse] = useState(false);
+    return (
+        <>
+            <TouchableOpacity
+                style={styles.buttonChannel}
+                onPress={() => {
+                    setCollapse(!collapse);
+                    console.log(collapse);
+                }}
+            >
+                <Text>ChannelName</Text>
+                <Icon
+                    name="plus"
+                    backgroundColor="#3b5998"
+                    size={20}
+                />
+            </TouchableOpacity>
+            <AnimatedView style={animation}>
+                <Text>Test</Text>
+            </AnimatedView>
+            <FlatList
+                data={data}
+                renderItem={({ item }) => (
+                    <>
+                        <Image
+                            style={{ width: 50, height: 50 }}
+                            source={{ uri: item.icon }}
+                        />
+                        <Text>{item.name}</Text>
+                    </>
+                )}
+                keyExtractor={(item) => item.id}
+            />
+        </>
+    );
+};
 const styles = StyleSheet.create({
+    buttonChannel: {
+        alignItems: "center",
+        backgroundColor: "red",
+        flexDirection: "row",
+        height: 70,
+        justifyContent: "space-around"
+    }
 });
 export default ChannelSelector;
--- src/screens/Dashboard/components/youTubeView.js
+++ src/screens/Dashboard/components/youTubeView.js
@@ -6,8 +6,6 @@ const YouTubeView = () => (
     <>
         <YouTube
             videoId="KVZ-P-ZI6W4" // The YouTube video ID
-            play // control playback of video with true/false
-            loop // control whether the video should loop when ended
             style={styles.youTubeView}
         />
     </>
