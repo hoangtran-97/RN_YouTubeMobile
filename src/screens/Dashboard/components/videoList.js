0ba80f23dabb3069114ca2f55e7e1e1af035303e
HoangTran <40290572+hoangtran-97@users.noreply.github.com>
2019-12-02 20:18:38 +0200

    mark as watched working

--- src/screens/Dashboard/components/videoList.js
+++ src/screens/Dashboard/components/videoList.js
@@ -3,9 +3,12 @@ import {
     Text, StyleSheet, FlatList, View
 } from "react-native";
 import { Thumbnail } from "react-native-thumbnail-video";
+import Icon from "react-native-vector-icons/FontAwesome";
 import colors from "../../../constants/colors";
 
-const VideoList = ({ channel, setPlaying }) => {
+const VideoList = ({
+    channel, watched, setCurrentIndex
+}) => {
     const { playlist, name } = channel;
     return (
         <View style={styles.container}>
@@ -15,17 +18,24 @@ const VideoList = ({ channel, setPlaying }) => {
                 {name}
             </Text>
             <FlatList
+                extraData={watched}
                 data={playlist}
-                renderItem={({ item }) => (
-                    <Thumbnail
-                        url={`https://www.youtube.com/watch?v=${item}`}
-                        showPlayIcon={false}
-                        imageWidth={200}
-                        imageHeight={150}
-                        onPress={() => setPlaying([item])}
-                    />
+                renderItem={({ item, index }) => (
+                    <View style={styles.itemContainer}>
+                        <Thumbnail
+                            url={`https://www.youtube.com/watch?v=${item}`}
+                            showPlayIcon={false}
+                            imageWidth={200}
+                            imageHeight={150}
+                            onPress={() => {
+                                setCurrentIndex(index);
+                                playVideoAtIndex(index);
+                            }}
+                        />
+                        <Icon name="eye" size={20} color={watched.includes(item) ? colors.text : colors.main} />
+                    </View>
                 )}
-                keyExtractor={(item, index) => index.toString()}
+                keyExtractor={(index) => index.toString()}
             />
         </View>
     );
@@ -34,6 +44,11 @@ const styles = StyleSheet.create({
     container: {
         paddingBottom: 500,
         paddingHorizontal: 20
+    },
+    itemContainer: {
+        alignItems: "center",
+        flexDirection: "row",
+        justifyContent: "space-around"
     },
     text: {
         color: colors.text,
--- src/screens/Dashboard/index.js
+++ src/screens/Dashboard/index.js
@@ -2,24 +2,54 @@ import React, { useState } from "react";
 import {
     StyleSheet, View
 } from "react-native";
-import YouTubeView from "./components/youTubeView";
+import YouTube from "react-native-youtube";
 import ChannelSelector from "./components/channelSelector";
 import VideoList from "./components/videoList";
 
 const DashboardScreen = ({ data }) => {
-    const [channel, setChannel] = useState({
-        id: 1,
-        name: "Meme Radar",
-        icon: "https://neverthink.tv/assets/images/63e3939725b3d92af5e7b8429a0f4d57e6be661abf380b39348f360e528dd6e2.png",
-        playlist: ["QSqIG5Dl-SM", "Km8kIX-8hVs", "85RhW75xM8U", "URLyBDYHoGo", "jM0GePXOdT0", "exLTGu_c5fs", "-goTfMUabxc", "y7pZzp99Jgs"]
-    });
+    const youTubeRef = React.createRef();
+    const [channel, setChannel] = useState(data[0]);
     const [playing, setPlaying] = useState(channel.playlist);
+    const [currentIndex, setCurrentIndex] = useState();
+    const [watched, setWatched] = useState([]);
+    playVideoAtIndex = (index) => {
+        youTubeRef.current.playVideoAt(index);
+    };
+    addToWatched = (index) => {
+        const videoId = playing[index];
+        const newWatched = watched;
+        newWatched.indexOf(videoId) === -1 ? newWatched.push(videoId) : null;
+        setWatched([...newWatched]);
+    };
     return (
         <>
-            <YouTubeView playing={playing} />
+            <YouTube
+                ref={youTubeRef}
+                videoIds={playing}
+                play
+                loop
+                style={styles.youTubeView}
+                onChangeState={(e) => {
+                    e.state === "playing" ? (
+                        youTubeRef.current.getVideosIndex().then((videosIndex) => {
+                            addToWatched(videosIndex);
+                        })
+                    ) : console.log(watched);
+                }}
+            />
             <View style={styles.container}>
-                <ChannelSelector data={data} channel={channel} setChannel={setChannel} setPlaying={setPlaying} />
-                <VideoList channel={channel} setPlaying={setPlaying} />
+                <ChannelSelector
+                    data={data}
+                    channel={channel}
+                    setChannel={setChannel}
+                    setPlaying={setPlaying}
+                />
+                <VideoList
+                    channel={channel}
+                    watched={watched}
+                    setCurrentIndex={setCurrentIndex}
+                    playVideoAtIndex={playVideoAtIndex}
+                />
             </View>
         </>
     );
@@ -27,6 +57,9 @@ const DashboardScreen = ({ data }) => {
 const styles = StyleSheet.create({
     container: {
         paddingHorizontal: 10
+    },
+    youTubeView: {
+        alignSelf: "stretch", height: 300
     }
 });
 export default DashboardScreen;
