59339987279081540cb893f1897029cbfcaef6e9
HoangTran <40290572+hoangtran-97@users.noreply.github.com>
2019-12-03 11:11:20 +0200

    root view color to black

--- ios/projectNeverThink/AppDelegate.m
+++ ios/projectNeverThink/AppDelegate.m
@@ -22,7 +22,7 @@
                                                    moduleName:@"projectNeverThink"
                                             initialProperties:nil];
 [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayback error: nil];
-  rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
+  rootView.backgroundColor = [[UIColor alloc] initWithRed:0.0f green:0.0f blue:0.0f alpha:1];
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
